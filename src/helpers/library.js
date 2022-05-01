d1b7c0d1e863deb20cf1125412ae0235d9b9bbce
ShivgunGaming <102505925+ShivgunGaming@users.noreply.github.com>
2022-05-01 11:16:29 +1000

    Create library.js

---
+++ src/helpers/library.js
@@ -0,0 +1,301 @@
+import SpidermanScene from "../helpers/SpidermanScene.png";
+import SpidermanThumb from "../helpers/SpidermanThumb.jpg";
+import SpidermanLogo from "../helpers/SpidermanLogo.png";
+import ThedarkknightScene from "../helpers/ThedarkknightScene.jpg";
+import ThedarkknightLogo from "../helpers/ThedarkknightLogo.jpg";
+import ThedarkknightThumb from "../helpers/ThedarkknightThumb.jpg";
+import AvengersLogo from "../helpers/AvengersLogo.png";
+import AvengersScene from "../helpers/AvengersScene.png";
+import AvengersThumb from "../helpers/AvengersThumb.jpg";
+import DuneThumb from "../helpers/DuneThumb.jpg";
+import DuneScene from "../helpers/DuneScene.jpg";
+import DuneLogo from "../helpers/DuneLogo.jpg";
+import LOTRThumb from "../helpers/LOTRThumb.jpg";
+import LOTRLogo from "../helpers/LOTRLogo.png";
+import SpidermanHLogo from "../helpers/SpidermanHLogo.png";
+import SpidermanHScene from "../helpers/SpidermanHScene.png";
+import SpidermanHThumb from "../helpers/SpidermanHThumb.jpg";
+import BBL from "../helpers/BBL.png";
+import BBS from "../helpers/BBS.jpg";
+import BBT from "../helpers/BBT.jpg";
+import KFPL from "../helpers/KFPL.png";
+import KFPS from "../helpers/KFPS.jpg";
+import KFPT from "../helpers/KFPT.jpg";
+import CapT from "../helpers/CapT.jpg";
+import CapS from "../helpers/CapS.jpg";
+import CapL from "../helpers/CapL.png";
+import GGL from "../helpers/GGL.png";
+import GGS from "../helpers/GGS.jpg";
+import GGT from "../helpers/GGT.jpg";
+import FFHT from "../helpers/FFHT.jpg";
+import FFHS from "../helpers/FFHS.jpg";
+import FFHL from "../helpers/FFHL.jpg";
+import IronT from "../helpers/IronT.jpg";
+import IronS from "../helpers/IronS.png";
+import IronL from "../helpers/IronL.jpg";
+import ThorT from "../helpers/ThorT.jpg";
+import ThorS from '../helpers/ThorS.png';
+import ThorL from "../helpers/ThorL.png";
+import SpideyT from "../helpers/SpideyT.jpg";
+import SpideyS from "../helpers/SpideyS.jpg";
+import SpideyL from "../helpers/SpideyL.jpg";
+import Spidey2T from "../helpers/Spidey2T.jpg";
+import Spidey2S from "../helpers/Spidey2S.jpg";
+import Spidey2L from "../helpers/Spidey2L.jpg";
+import LOTRScene from "../helpers/LOTRScene.png";
+
+export const movies = [
+    {
+        Movie: "",
+        Thumnbnail: SpidermanThumb,
+        Scene: SpidermanScene,
+        Logo: "",
+        Name: "Spiderman No Way Home",
+        Description: "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
+        Genre: "Adventure, Action, Fantasy",
+        Stars: "Tom Holland, Zendaya, Benedict Cumberbatch",
+        Year: "2021",
+        Duration: "2h 28min",
+        Rating: "8.4/10",
+        Director: "Jon Watts",
+        Writers: "Stan Lee, Erik Sommers, Chris McKenna",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: ThedarkknightThumb,
+        Scene: ThedarkknightScene,
+        Logo: ThedarkknightLogo,
+        Name: "The Dark Knight",
+        Description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        Genre: "Crime, Action, Drama",
+        Stars: "Christian Bale, Heath Ledger, Aaron Eckhart, Gary Oldman",
+        Year: "2008",
+        Duration: "2h 32min",
+        Rating: "9/10",
+        Director: "Christopher Nolan",
+        Writers: "Christopher Nolan, Jonathan Nolan, David S. Goyer",
+        ParentsGuide: "PG-13"
+    },
+
+    {  
+        Movie: "",
+        Thumnbnail: AvengersThumb,
+        Scene: AvengersScene,
+        Logo: AvengersLogo,
+        Name: "Avengers Infinity War",
+        Description: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Too Many",
+        Year: "2018",
+        Duration: "2h 29min",
+        Rating: "8.5/10",
+        Director: "Anthony Russo, Joe Russo",
+        Writers: "Stan Lee, Stephen McFelly, Christopher Markus",
+        ParentsGuide: "PG-13"
+    },
+
+    {
+        Movie: "",
+        Thumnbnail: DuneThumb,
+        Scene: DuneScene,
+        Logo: DuneLogo,
+        Name: "Dune",
+        Description: "A noble family becomes embroiled in a war for control over the galaxy's most valuable asset while its heir becomes troubled by visions of a dark future.",
+        Genre: "Adventure, Action, Drama",
+        Stars: "Rebecca Ferguson, Timothee Chalamet, Zendaya",
+        Year: "2021",
+        Duration: "2h 35min",
+        Rating: "8.1/10",
+        Director: "Denis Villeneuve",
+        Writers: "Denis Villeneuve, Eric Roth, Jon Spaihts",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: LOTRThumb,
+        Scene: LOTRScene,
+        Logo: "",
+        Name: "The Lord Of The Rings: The Fellowship Of The Ring",
+        Description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+        Genre: "Adventure, Action, Drama",
+        Stars: "Elijah Wood, Ian McKellen, Orlando Bloom",
+        Year: "2001",
+        Duration: "2h 58min",
+        Rating: "8.8/10",
+        Director: "Peter Jackson",
+        Writers: "Fran Walsh, Philippa Boyens, J.R.R Tolkien",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: SpidermanHThumb,
+        Scene: SpidermanHScene,
+        Logo: SpidermanHLogo,
+        Name: "Spiderman Homecoming",
+        Description: "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Tom Holland, Michael Keaton, Robert Downey Jr. ",
+        Year: "2017",
+        Duration: "2h 13min",
+        Rating: "7.4/10",
+        Director: "Jon Watts",
+        Writers: "Jon Watts, John Francis Daley, Jonathan Goldstein",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: BBT,
+        Scene: BBS,
+        Logo: BBL,
+        Name: "The Hobbit The Desolation Of Smaug",
+        Description: "The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim Erebor, their homeland, from Smaug. Bilbo Baggins is in possession of a mysterious and magical ring.",
+        Genre: "Adventure, Fantasy",
+        Stars: "Martain Freeman, Ian McKellen, Richard Armitage",
+        Year: "2013",
+        Duration: "2h 41min",
+        Rating: "7.8/10",
+        Director: "Peter Jackson",
+        Writers: "Fran Walsh, Philippa Boyens, Peter Jackson",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: KFPT,
+        Scene: KFPS,
+        Logo: KFPL,
+        Name: "Kung Fu Panda",
+        Description: "To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.",
+        Genre: "Animation, Action, Adventure",
+        Stars: "Jack Black, Ian McShane, Angelina Jolie",
+        Year: "2008",
+        Duration: "1h 32min",
+        Rating: "7.6/10",
+        Director: "Mark Osborne, John Stevenson",
+        Writers: "Jonathan Aibel, Glenn Berger, Ethan Reiff",
+        ParentsGuide: "PG"
+    },
+
+    {
+        Movie: "",
+        Thumnbnail: CapT,
+        Scene: CapS,
+        Logo: CapL,
+        Name: "Captain America The Winter Soldier",
+        Description: "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Chris Evans, Samuel L Jackson, Scarlett Johansson",
+        Year: "2014",
+        Duration: "2h 16min",
+        Rating: "7.8/10",
+        Director: "Anthony Russo, Joe Russo",
+        Writers: "Christopher Markus, Stephen McFeely, Joe Simon",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: GGT,
+        Scene: GGS,
+        Logo: GGL,
+        Name: "Guardians of the Galaxy",
+        Description: "A group of intergalactic criminals must pull together to stop a fanatical warrior with plans to purge the universe.",
+        Genre: "Adventure, Action, Comedy",
+        Stars: "Chris Prat, Vin Diesel, Bradley Cooper",
+        Year: "2014",
+        Duration: "2h 1min",
+        Rating: "8.0/10",
+        Director: "James Gunn",
+        Writers: "James Gunn, Nicole Perlman, Dan Abnett",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: FFHT,
+        Scene: FFHS,
+        Logo: FFHL,
+        Name: "Spiderman Far From Home",
+        Description: "Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Tom Holland, Zendaya, Samuel L Jackson, Jake Gyllenhaal",
+        Year: "2019",
+        Duration: "2h 9min",
+        Rating: "7.4/10",
+        Director: "Jon Watts",
+        Writers: "Stan Lee, Erik Sommers, Chris McKenna",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: IronT,
+        Scene: IronS,
+        Logo: "",
+        Name: "Iron Man 2",
+        Description: "With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father's legacy.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Robert Downey Jr., Mickie Rourke, Gwyneth Paltrow",
+        Year: "2010",
+        Duration: "2h 4min",
+        Rating: "6.9/10",
+        Director: "Jon Favreau",
+        Writers: "Stan Lee, Don Heck, Justin Theroux",
+        ParentsGuide: "PG-13"
+    },
+
+    {   Movie: "",
+        Thumnbnail: ThorT,
+        Scene: ThorS,
+        Logo: "",
+        Name: "Thor Ragnarok",
+        Description: "Imprisoned on the planet Sakaar, Thor must race against time to return to Asgard and stop Ragnarök, the destruction of his world, at the hands of the powerful and ruthless villain Hela.",
+        Genre: "Adventure, Action, Comedy",
+        Stars: "Chris Hemsworth, Tom Hiddleston, Cate Blanchett",
+        Year: "2017",
+        Duration: "2h 10min",
+        Rating: "7.9/10",
+        Director: "Taika Waititi",
+        Writers: "Craig Kyle, Eric Pearson, Christopher L. Yost",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: SpideyT,
+        Scene: SpideyS,
+        Logo: "",
+        Name: "Spidermam",
+        Description: "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Tobey Maguire, Kirsten Dunst, Willen Dafoe",
+        Year: "2002",
+        Duration: "2h 1min",
+        Rating: "7.4/10",
+        Director: "Sam Raimi",
+        Writers: "Stan Lee, Steve Ditko, David Koepp",
+        ParentsGuide: "PG-13"
+    },
+
+    { 
+        Movie: "",
+        Thumnbnail: Spidey2T,
+        Scene: Spidey2S,
+        Logo: Spidey2L,
+        Name: "Spidermam 2",
+        Description: "Peter Parker is beset with troubles in his failing personal life as he battles a brilliant scientist named Doctor Otto Octavius.",
+        Genre: "Adventure, Action, Sci-Fi",
+        Stars: "Tobey Maguire, Kirsten Dunst, Alfred Molina",
+        Year: "2004",
+        Duration: "2h 7min",
+        Rating: "7.4/10",
+        Director: "Sam Raimi",
+        Writers: "Stan Lee, Steve Ditko, Alfred Gough",
+        ParentsGuide: "PG-13"
+    },
+];
